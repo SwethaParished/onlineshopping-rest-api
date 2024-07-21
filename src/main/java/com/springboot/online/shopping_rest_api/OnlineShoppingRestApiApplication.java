2376684e53862c4ec9a5bc833412da3eeef4f8f5
“Swetha <swethaparished06@gmail.com>
2024-07-20 19:38:47 -0500

    Started the project onlineshopping-rest-api

---
+++ src/main/java/com/springboot/online/shopping_rest_api/OnlineShoppingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.online.shopping_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingRestApiApplication.class, args);
+	}
+
+}
